94edc6e9ccfa65cba6741c5f994d365486e3dc57
Vamsi Krishna V <vamsi@vonectech.com>
2024-07-13 23:02:38 +0530

    nice

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,64 @@
 <template>
-  <div class="bg-cover bg-gray-800" style="background-image:url(/images/bg/body-bg.webp);">
+  <div
+    class="bg-cover bg-gray-800"
+    style="background-image: url(/images/bg/body-bg.webp)"
+  >
+    <HeaderSection />
 
-    <HeaderSection/>
+    <OffCanvasMobileMenu
+      :class="{ 'show-mobile-menu': navOpen }"
+      @togglenav="navOpen = !navOpen"
+    />
 
-    <OffCanvasMobileMenu :class="{'show-mobile-menu' : navOpen}" @togglenav="navOpen = !navOpen" />
+    <HeroBanner />
 
-    <HeroBanner/>
+    <GamingWorld />
 
-    <GamingWorld/>
+    <LiveStream />
 
-    <LiveStream/>
+    <UpcomingMatchesHome />
 
-    <UpcomingMatchesHome/>
+    <CounterUp />
 
-    <CounterUp/>
+    <PopularGame />
 
-    <PopularGame/>
+    <Testimonial />
 
-    <Testimonial/>
+    <BlogPost />
 
-    <BlogPost/>
+    <ContactBanner :paddingTop="paddingTop" />
 
-    <ContactBanner :paddingTop="paddingTop"/>
-
-    <Footer/>
-
+    <Footer />
   </div>
 </template>
 
 <script>
 export default {
   components: {
-      HeaderSection: () => import('@/components/HeaderSection'),
-      HeroBanner: () => import('@/components/HeroBanner'),
-      OffCanvasMobileMenu: () => import('@/components/Header/OffCanvasMobileMenu'),
-      GamingWorld: () => import('@/components/GamingWorld'),
-      LiveStream: () => import('@/components/LiveStream'),
-      UpcomingMatchesHome: () => import('@/components/UpcomingMatchesHome'),
-      CounterUp: () => import('@/components/CounterUp'),
-      PopularGame: () => import('@/components/Games/PopularGame'),
-      Testimonial: () => import('@/components/Testimonial'),
-      BlogPost: () => import('@/components/BlogPost'),
-      ContactBanner: () => import('@/components/ContactBanner'),
-      Footer: () => import('@/components/Footer')
-  }, 
-  data() {
-      return {
-          navOpen: false,
-          paddingTop: "pt-32"
-      }
+    HeaderSection: () => import("@/components/HeaderSection"),
+    HeroBanner: () => import("@/components/HeroBanner"),
+    OffCanvasMobileMenu: () =>
+      import("@/components/Header/OffCanvasMobileMenu"),
+    GamingWorld: () => import("@/components/GamingWorld"),
+    LiveStream: () => import("@/components/LiveStream"),
+    UpcomingMatchesHome: () => import("@/components/UpcomingMatchesHome"),
+    CounterUp: () => import("@/components/CounterUp"),
+    PopularGame: () => import("@/components/Games/PopularGame"),
+    Testimonial: () => import("@/components/Testimonial"),
+    BlogPost: () => import("@/components/BlogPost"),
+    ContactBanner: () => import("@/components/ContactBanner"),
+    Footer: () => import("@/components/Footer"),
   },
-}
+  head() {
+    return {
+      title: "Home Page",
+    };
+  },
+  data() {
+    return {
+      navOpen: false,
+      paddingTop: "pt-32",
+    };
+  },
+};
 </script>
