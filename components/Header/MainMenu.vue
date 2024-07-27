56c00ea097c2d414d1f9c70aa4950a4467b78f90
Vamsi Krishna V <vamsi@vonectech.com>
2024-07-27 12:39:22 +0000

    minor

--- components/Header/MainMenu.vue
+++ components/Header/MainMenu.vue
@@ -8,7 +8,61 @@
                 </li>
                 <li class="relative mr-14 group z-10 before:bg-nav-shape before:content-[''] before:absolute before:w-23.5 before:h-11 before:z-n1 before:top-1/2 before:left-1/2 before:transform before:-translate-x-2/4 before:-translate-y-2/4 before:transition-all before:opacity-0 hover:before:opacity-100">
 
-                    <NuxtLink to="/" class="text-white uppercase font-bold font-exo relative">Match</NuxtLink>
+                    <NuxtLink to="/" class="text-white uppercase font-bold font-exo relative">Community</NuxtLink>
+                    <ul class="flex flex-col bg-white absolute w-56 px-5 py-5 rounded left-0 right-auto top-12 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-all group-hover:top-9">
+                        <li>
+                            <NuxtLink to="/match" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">About TEC</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/match/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Games</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/match/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Arena</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/match/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">News</NuxtLink>
+                        </li>
+                    </ul>
+
+                </li>
+                <li class="relative mr-14 group z-10 before:bg-nav-shape before:content-[''] before:absolute before:w-23.5 before:h-11 before:z-n1 before:top-1/2 before:left-1/2 before:transform before:-translate-x-2/4 before:-translate-y-2/4 before:transition-all before:opacity-0 hover:before:opacity-100">
+
+                    <NuxtLink to="/" class="text-white uppercase font-bold font-exo relative">Events</NuxtLink>
+                    <ul class="flex flex-col bg-white absolute w-56 px-5 py-5 rounded left-0 right-auto top-12 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-all group-hover:top-9">
+                        <li>
+                            <NuxtLink to="/match" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Upcoming</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/match/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Past Events</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/match/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Live Streams</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/match/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Event Calendar</NuxtLink>
+                        </li>
+                    </ul>
+
+                </li>
+                <li class="relative mr-14 group z-10 before:bg-nav-shape before:content-[''] before:absolute before:w-23.5 before:h-11 before:z-n1 before:top-1/2 before:left-1/2 before:transform before:-translate-x-2/4 before:-translate-y-2/4 before:transition-all before:opacity-0 hover:before:opacity-100">
+
+                    <NuxtLink to="/" class="text-white uppercase font-bold font-exo relative">Resources</NuxtLink>
+                    <ul class="flex flex-col bg-white absolute w-56 px-5 py-5 rounded left-0 right-auto top-12 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-all group-hover:top-9">
+                        <li>
+                            <NuxtLink to="/match" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Training</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/match/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Tips and Tricks</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/match/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Resources</NuxtLink>
+                        </li>
+                    </ul>
+
+                </li>
+                <li class="relative mr-14 group z-10 before:bg-nav-shape before:content-[''] before:absolute before:w-23.5 before:h-11 before:z-n1 before:top-1/2 before:left-1/2 before:transform before:-translate-x-2/4 before:-translate-y-2/4 before:transition-all before:opacity-0 hover:before:opacity-100">
+
+                    <NuxtLink to="/" class="text-white uppercase font-bold font-exo relative">News</NuxtLink>
                     <ul class="flex flex-col bg-white absolute w-56 px-5 py-5 rounded left-0 right-auto top-12 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-all group-hover:top-9">
                         <li>
                             <NuxtLink to="/match" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Match Page</NuxtLink>
@@ -19,69 +73,7 @@
                     </ul>
 
                 </li>
-                <li class="relative mr-14 group z-10 before:bg-nav-shape before:content-[''] before:absolute before:w-23.5 before:h-11 before:z-n1 before:top-1/2 before:left-1/2 before:transform before:-translate-x-2/4 before:-translate-y-2/4 before:transition-all before:opacity-0 hover:before:opacity-100">
-
-                    <NuxtLink to="/" class="text-white uppercase font-bold font-exo relative">Pages</NuxtLink>
-                    <ul class="flex flex-col bg-white absolute w-56 px-5 py-5 rounded left-0 right-auto top-12 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-all group-hover:top-9">
-                        <li>
-                            <NuxtLink to="/about" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">About Us</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/game" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">All Games</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/game/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Game Details</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/player" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Players</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/player/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Player Details</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/battleteam" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Battle Teams</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/battle/gamer" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Single Battle</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/FAQ" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">FAQ</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/404Error" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">404-Error</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/signup" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">SignUp</NuxtLink>
-                        </li>
-                    </ul>
-
-                </li>
-                <li class="relative mr-14 group z-10 before:bg-nav-shape before:content-[''] before:absolute before:w-23.5 before:h-11 before:z-n1 before:top-1/2 before:left-1/2 before:transform before:-translate-x-2/4 before:-translate-y-2/4 before:transition-all before:opacity-0 hover:before:opacity-100">
-                    <NuxtLink to="/" class="text-white uppercase font-bold font-exo relative">Blog</NuxtLink>
-                    <ul class="flex flex-col bg-white absolute w-56 px-5 py-5 rounded left-0 right-auto top-12 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-all group-hover:top-9">
-                        <li>
-                            <NuxtLink to="/blog-without-sidebar" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Blog Without Sidebar Page</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/blog-right-sidebar" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Blog Right Sidebar</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/blog-left-sidebar" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Blog Left Sidebar</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/blog-grid-right-sidebar" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Blog Grid Right Sidebar</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/blog-grid-left-sidebar" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Blog Grid Left Sidebar</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/blog-grid-without-sidebar" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Blog Grid Without Sidebar</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/blog/1" class="text-sm font-medium block leading-8 capitalize text-black transition-all hover:text-primary">Blog Details</NuxtLink>
-                        </li>
-                    </ul>
-                </li>
+                
                 <li class="relative mr-14 z-10 before:bg-nav-shape before:content-[''] before:absolute before:w-23.5 before:h-11 before:z-n1 before:top-1/2 before:left-1/2 before:transform before:-translate-x-2/4 before:-translate-y-2/4 before:transition-all before:opacity-0 hover:before:opacity-100">
                     <NuxtLink to="/contact" class="text-white uppercase font-bold font-exo relative">Contact</NuxtLink>
                 </li>
