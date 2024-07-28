4f447da31b87474d19b1d05f27e9ffe9c31b6fef
Vamsi Krishna V <vamsi@vonectech.com>
2024-07-28 22:50:03 +0530

    changes

--- pages/game/_slug.vue
+++ pages/game/_slug.vue
@@ -1,48 +1,57 @@
 <template>
-    <div class="bg-cover bg-gray-800" style="background-image:url(/images/bg/body-bg2.webp);">
+  <div
+    class="bg-cover bg-gray-800"
+    style="background-image: url(/images/bg/body-bg2.webp)"
+  >
+    <HeaderSection />
 
-        <HeaderSection/>
+    <OffCanvasMobileMenu
+      :class="{ 'show-mobile-menu': navOpen }"
+      @togglenav="navOpen = !navOpen"
+    />
 
-        <OffCanvasMobileMenu :class="{'show-mobile-menu' : navOpen}" @togglenav="navOpen = !navOpen" />
+    <Breadcrumb
+      :BreadcrumbTitle="BreadcrumbTitle"
+      :BreadcrumbSubTitle="BreadcrumbSubTitle"
+    />
 
-        <Breadcrumb :BreadcrumbTitle="BreadcrumbTitle" :BreadcrumbSubTitle="BreadcrumbSubTitle"/>
+    <GameDetails :games="gameHome" />
 
-        <GameDetails :games="gameHome"/>
+    <ContactBanner :paddingTop="paddingTop" />
 
-        <ContactBanner :paddingTop="paddingTop"/>
-
-        <Footer/>
-
-    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
-
-    import gameHome from '@/data/games.json'
-    export default {
-        components: {
-            GameDetails: () => import('@/components/Games/GameDetails'),
-            HeaderSection: () => import('@/components/HeaderSection'),
-            OffCanvasMobileMenu: () => import('@/components/Header/OffCanvasMobileMenu'),
-            Breadcrumb: () => import('@/components/Breadcrumb'),
-            CounterUp: () => import('@/components/CounterUp'),
-            ContactBanner: () => import('@/components/ContactBanner'),
-            Footer: () => import('@/components/Footer')
-        },
-        data() {
-            return {
-                btnName: "JOIN NOW",
-                gameHome,
-                navOpen: false,
-                BreadcrumbTitle: "Game Details",
-                BreadcrumbSubTitle: "Game",
-                paddingTop: "pt-32",
-                slug: this.$route.params.slug
-            };
-        },
-        mounted () {
-            this.gameHome = gameHome.find(games => games.slug == this.$route.params.slug);
-        }
-    }
-
+import gameHome from "@/data/games.json";
+export default {
+  components: {
+    GameDetails: () => import("@/components/Games/GameDetails"),
+    HeaderSection: () => import("@/components/HeaderSection"),
+    OffCanvasMobileMenu: () =>
+      import("@/components/Header/OffCanvasMobileMenu"),
+    Breadcrumb: () => import("@/components/Breadcrumb"),
+    CounterUp: () => import("@/components/CounterUp"),
+    ContactBanner: () => import("@/components/ContactBanner"),
+    Footer: () => import("@/components/Footer"),
+  },
+  data() {
+    return {
+      btnName: "JOIN NOW",
+      gameHome,
+      navOpen: false,
+      BreadcrumbTitle: "Game Details",
+      BreadcrumbSubTitle: "Game",
+      paddingTop: "pt-32",
+      slug: this.$route.params.slug,
+    };
+  },
+  mounted() {
+    this.gameHome = gameHome.find(
+      (games) => games.slug == this.$route.params.slug
+    );
+    this.BreadcrumbTitle = this.gameHome.title;
+  },
+};
 </script>
