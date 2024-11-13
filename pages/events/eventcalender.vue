5b5feb2d4f711c1db6f92fb21ac8acba81843b2d
Vamsi Krishna V <vamsi@vonectech.com>
2024-11-13 06:11:35 +0530

    typo

--- pages/events/eventcalender.vue
+++ pages/events/eventcalender.vue
@@ -1,54 +1,52 @@
 <template>
-    <div
-      class="bg-cover bg-gray-800"
-      style="background-image: url(/images/bg/body-bg2.webp)"
-    >
-      <HeaderSection />
-  
-      <OffCanvasMobileMenu
-        :class="{ 'show-mobile-menu': navOpen }"
-        @togglenav="navOpen = !navOpen"
-      />
-  
-      <Breadcrumb
-        :BreadcrumbTitle="BreadcrumbTitle"
-        :BreadcrumbSubTitle="BreadcrumbSubTitle"
-      />
-  
-      <PopularGame />
+  <div
+    class="bg-cover bg-gray-800"
+    style="background-image: url(/images/bg/body-bg2.webp)"
+  >
+    <HeaderSection />
 
-      <EventCalender />
-  
-      <ContactBanner :paddingTop="paddingTop" />
-  
-      <Footer />
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    components: {
-      HeaderSection: () => import("@/components/HeaderSection"),
-      OffCanvasMobileMenu: () =>
-        import("@/components/Header/OffCanvasMobileMenu"),
-      Breadcrumb: () => import("@/components/Breadcrumb"),
-      ContactBanner: () => import("@/components/ContactBanner"),
-      Footer: () => import("@/components/Footer"),
+    <OffCanvasMobileMenu
+      :class="{ 'show-mobile-menu': navOpen }"
+      @togglenav="navOpen = !navOpen"
+    />
 
-    },
-    data() {
-      return {
-        navOpen: false,
-        BreadcrumbTitle: "Event Calender",
-        BreadcrumbSubTitle: "Event Calender",
-        paddingTop: "pt-32",
-      };
-    },
-    head() {
-      return {
-        title: "Event Calender",
-      };
-    },
-  };
-  </script>
-  
+    <Breadcrumb
+      :BreadcrumbTitle="BreadcrumbTitle"
+      :BreadcrumbSubTitle="BreadcrumbSubTitle"
+    />
+
+    <PopularGame />
+
+    <EventCalender />
+
+    <ContactBanner :paddingTop="paddingTop" />
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    HeaderSection: () => import("@/components/HeaderSection"),
+    OffCanvasMobileMenu: () =>
+      import("@/components/Header/OffCanvasMobileMenu"),
+    Breadcrumb: () => import("@/components/Breadcrumb"),
+    ContactBanner: () => import("@/components/ContactBanner"),
+    Footer: () => import("@/components/Footer"),
+  },
+  data() {
+    return {
+      navOpen: false,
+      BreadcrumbTitle: "Event Calender",
+      BreadcrumbSubTitle: "Event Calender",
+      paddingTop: "pt-32",
+    };
+  },
+  head() {
+    return {
+      title: "Event Calender",
+    };
+  },
+};
+</script>
