8fd336795814c89dae029c665bc491ec7088ae1d
Vamsi Krishna V <vamsi@vonectech.com>
2024-11-18 15:10:55 +0530

    Update PrimaryButton.vue

--- components/Button/PrimaryButton.vue
+++ components/Button/PrimaryButton.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- Button Start -->
   <NuxtLink
-    to="/signup"
+    to="/contact"
     style="background-image: url(/images/others/btn-bg.png)"
     class="w-230 md:text-2xl md:leading-12 h-73 flex items-center text-white font-bold justify-center font-exo group hover:opacity-80 leading-70 text-xl"
     >Join US
-    
+
     <img
       class="ml-3 w-5 h-5 group-hover:ml-4 transition-all"
       src="/images/icon/arrrow-icon.webp"
